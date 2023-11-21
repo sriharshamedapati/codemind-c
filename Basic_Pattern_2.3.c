6337de7eb221ebd2e09b2d552ea74613d63b57ed
sriharshamedapati <147472612+sriharshamedapati@users.noreply.github.com>
2023-11-21 21:49:05 +0530

    #include<stdio.h> int main(){     int n,num=65;     scanf("%d",&n);     for(int i=1;i<=n;i++){
            for(int j=1;j<=i;j++){
                char character=(char)num;
                printf("%c ",character);
                num++;
            }
            num=65;
            printf("
    ");
        }
        return 0;
    }

---
+++ Basic_Pattern_2.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,num=65;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        
+        for(int j=1;j<=i;j++){
+            char character=(char)num;
+            printf("%c ",character);
+            num++;
+        }
+        num=65;
+        printf("
+");
+    }
+    return 0;
+}
