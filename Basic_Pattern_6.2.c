7d8fca11cd0302b3439ef53553181e5cd447269d
sriharshamedapati <147472612+sriharshamedapati@users.noreply.github.com>
2023-11-29 10:52:43 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     for(int i=n ; i>0 ; i--){         for(int j=1 ; j<=n ; j++){             if(j<i){                 printf(" ");             }else{                 printf("%d ",i);             }         }         printf(" ");     }     return 0; }

---
+++ Basic_Pattern_6.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=n ; i>0 ; i--){
+        for(int j=1 ; j<=n ; j++){
+            if(j<i){
+                printf(" ");
+            }else{
+                printf("%d ",i);
+            }
+        }
+        printf("
+");
+    }
+    return 0;
+}
