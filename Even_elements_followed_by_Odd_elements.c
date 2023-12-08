cade564076509d6a68fbd2233cc96be64046d6e2
sriharshamedapati <147472612+sriharshamedapati@users.noreply.github.com>
2023-12-08 19:05:52 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int A[n];     for(int i=0;i<n;i++){         scanf("%d",&A[i]);         if(A[i]%2==0){             printf("%d ",A[i]);         }     }     for(int i=0;i<n;i++){         if(A[i]%2!=0){             printf("%d ",A[i]);         }     }     return 0; }

---
+++ Even_elements_followed_by_Odd_elements.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int A[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&A[i]);
+        if(A[i]%2==0){
+            printf("%d ",A[i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(A[i]%2!=0){
+            printf("%d ",A[i]);
+        }
+    }
+    return 0;
+}
